591599d13bf05a16510ca04928e678f12e14b953
Victor Elvis Lorenzo <79326875+Vic-developers@users.noreply.github.com>
2023-11-23 13:54:53 -0400

    Proyecto de Laboratorios
    Debes agregar los módulos de node con npm i para que aparezcan en el proyecto

---
+++ IsfoPlanner/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavegacionComponent } from './navegacion/navegacion.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { LaboratoriosComponent } from './laboratorios/laboratorios.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { EquiposComponent } from './equipos/equipos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavegacionComponent,
+    DashboardComponent,
+    LaboratoriosComponent,
+    UsuariosComponent,
+    EquiposComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
